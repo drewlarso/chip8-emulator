ff5b19050f34432bae1ca19469a0612821fa27f4
drewlarso <drewlarso1@gmail.com>
2025-10-06 22:22:01 -0600

    add init and update functions to processor

--- main.go
+++ main.go
@@ -12,21 +12,29 @@ import (
 const ScreenWidth int = 64
 const ScreenHeight int = 32
 const WindowScale int = 10
+const CyclesPerFrame int = 10
 
 func main() {
 	rl.InitWindow(int32(ScreenWidth*WindowScale), int32(ScreenHeight*WindowScale), "Chip8 Emulator")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
-	disp := display.Display{}
-	cpu := processor.Processor{}
+	disp := display.Display{} // disp is the actual value
+	cpu := processor.NewProcessor()
 	fmt.Println(cpu)
-	kb := keyboard.NewKeyboard()
+	kb := keyboard.NewKeyboard() // already a pointer to my keyboard struct
 
 	disp.Set(532, true)
 
 	for !rl.WindowShouldClose() {
 		kb.Update()
+
+		for range CyclesPerFrame {
+			cpu.Cycle(&disp, kb)
+		}
+
+		cpu.UpdateTimers()
+
 		disp.Draw()
 	}
 }
--- processor/processor.go
+++ processor/processor.go
@@ -1,5 +1,10 @@
 package processor
 
+import (
+	"github.com/drewlarso/chip8-emulator/display"
+	"github.com/drewlarso/chip8-emulator/keyboard"
+)
+
 type Processor struct {
 	registers     [16]byte
 	indexRegister uint16
@@ -8,4 +13,27 @@ type Processor struct {
 	pc            uint16
 	sp            byte
 	stack         [16]uint16
+}
+
+func NewProcessor() *Processor {
+	return &Processor{
+		registers:     [16]byte{},
+		indexRegister: 0,
+		delayTimer:    255,
+		soundTimer:    255,
+		pc:            0,
+		sp:            0,
+		stack:         [16]uint16{},
+	}
+}
+
+func (p *Processor) Cycle(display *display.Display, keyboard *keyboard.Keyboard) {}
+
+func (p *Processor) UpdateTimers() {
+	if p.delayTimer != 0 {
+		p.delayTimer--
+	}
+	if p.soundTimer != 0 {
+		p.soundTimer--
+	}
 }
